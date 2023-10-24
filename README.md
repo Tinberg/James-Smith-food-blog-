112f18f2ac8ba45f021157d9fbd63a2e93673d0d
Mathias Tinberg <mathias@Mathias-sin-MacBook-Pro.local>
2023-10-24 14:18:01 +0200

    about me

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ School project HTML/CSS/JS/HeadlessCMS
 - h1 for brand name and logo on every page bcs of text size hierarchy
 
 -i used some styles in global several times,(not DRY) bcs there was some content that i used on all of my pages, and in that way i didnt have to put on to many classes to the elements. 
+
+
+fix smooth transition på about me og error if image ikke loader. 
--- js/index.js
+++ js/index.js
@@ -1,5 +1,25 @@
 // Newsletter Form
-document.querySelector('.newsletter-form').addEventListener('submit', function(e) {
+document
+  .querySelector(".newsletter-form")
+  .addEventListener("submit", function (e) {
     e.preventDefault();
-    alert('Thank you for signing up!');
+    alert("Thank you for signing up!");
+  });
+
+document.addEventListener("DOMContentLoaded", function () {
+  let figures = document.querySelectorAll(".slider figure");
+
+  if (!figures.length) {
+    console.error("Figures not found!");
+    return;
+  }
+
+  let currentFigureIndex = 0;
+  figures[currentFigureIndex].classList.add("active");
+
+  setInterval(function () {
+    figures[currentFigureIndex].classList.remove("active");
+    currentFigureIndex = (currentFigureIndex + 1) % figures.length;
+    figures[currentFigureIndex].classList.add("active");
+  }, 3000);
 });
