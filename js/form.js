94378f858d1976e18b66f79bd1e900045836dd57
Mathias Tinberg <mathias@Mathias-sin-MacBook-Pro.local>
2023-11-29 13:32:42 +0100

    Form to wordpress

--- js/form.js
+++ js/form.js
@@ -78,8 +78,7 @@
 //   }
 // });
 
-
-// Form index.html and contact.html 
+// -------Form  validation index.html and contact.html
 document.addEventListener("DOMContentLoaded", function () {
   const newsletterForm = document.getElementById("newsletterForm");
   const contactForm = document.getElementById("contactForm");
@@ -95,114 +94,99 @@ document.addEventListener("DOMContentLoaded", function () {
       validateForm(event, "contact");
     });
   }
-//this funtion validate and handle form submission
-function validateForm(event, formType) {
-  event.preventDefault(); 
-  let hasError = false;
-  //select inputfields name based on formtype
-  const nameInput = formType === "newsletter" ? "name" : "contactName";
-  const emailInput = formType === "newsletter" ? "email" : "contactEmail";
-  const name = event.target.querySelector(`#${nameInput}`).value;
-  const email = event.target.querySelector(`#${emailInput}`).value;
+  //this funtion validate form inputs and display error message
+  function validateForm(event, formType) {
+    event.preventDefault();
+    let hasError = false;
+    //select inputfields name based on formtype
+    const nameInput = formType === "newsletter" ? "name" : "contactName";
+    const emailInput = formType === "newsletter" ? "email" : "contactEmail";
+    const name = event.target.querySelector(`#${nameInput}`).value;
+    const email = event.target.querySelector(`#${emailInput}`).value;
 
-  // This validate name for both(more then 5 characters)
-  if (name.length <= 5) {
-    event.target.querySelector(`#${nameInput}Error`).innerText =
-      "Name must be more than 5 characters long.";
-    hasError = true;
-  } else {
-    event.target.querySelector(`#${nameInput}Error`).innerText = "";
-  }
-
-  // Validate email for both(pattern)
-  const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-  if (!emailPattern.test(email)) {
-    event.target.querySelector(`#${emailInput}Error`).innerText =
-      "Please provide a valid email address.";
-    hasError = true;
-  } else {
-    event.target.querySelector(`#${emailInput}Error`).innerText = "";
-  }
-
-  // Validation subject and message for contact form (more then 15 characters  and more then 25 characters)
-  if (formType === "contact") {
-    const subject = event.target.querySelector("#contactSubject").value;
-    const message = event.target.querySelector("#contactMessage").value;
-
-    if (subject.length <= 15) {
-      event.target.querySelector("#contactSubjectError").innerText =
-        "Subject must be more than 15 characters long.";
+    // This validate name for both(more then 5 characters)
+    if (name.length <= 5) {
+      event.target.querySelector(`#${nameInput}Error`).innerText =
+        "Name must be more than 5 characters long.";
       hasError = true;
     } else {
-      event.target.querySelector("#contactSubjectError").innerText = "";
+      event.target.querySelector(`#${nameInput}Error`).innerText = "";
     }
 
-    if (message.length <= 25) {
-      event.target.querySelector("#contactMessageError").innerText =
-        "Message must be more than 25 characters long.";
+    // Validate email for both(pattern)
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailPattern.test(email)) {
+      event.target.querySelector(`#${emailInput}Error`).innerText =
+        "Please provide a valid email address.";
       hasError = true;
     } else {
-      event.target.querySelector("#contactMessageError").innerText = "";
+      event.target.querySelector(`#${emailInput}Error`).innerText = "";
     }
 
-    // If no error, post data to WordPress
-    if (!hasError) {
-      
-      postContactFormDataToWordPress(name, email, subject, message);
+    // Validation subject and message for contact form (more then 15 characters  and more then 25 characters)
+    if (formType === "contact") {
+      const subject = event.target.querySelector("#contactSubject").value;
+      const message = event.target.querySelector("#contactMessage").value;
+
+      if (subject.length <= 15) {
+        event.target.querySelector("#contactSubjectError").innerText =
+          "Subject must be more than 15 characters long.";
+        hasError = true;
+      } else {
+        event.target.querySelector("#contactSubjectError").innerText = "";
+      }
+
+      if (message.length <= 25) {
+        event.target.querySelector("#contactMessageError").innerText =
+          "Message must be more than 25 characters long.";
+        hasError = true;
+      } else {
+        event.target.querySelector("#contactMessageError").innerText = "";
+      }
+
+      // If no error, post data to WordPress
+      if (!hasError) {
+        postContactFormDataToWordPress(name, email, subject, message);
+      }
+    } else if (formType === "newsletter" && !hasError) {
+      window.location.href = "/html/newsletter-thank-you.html";
     }
-  } else if (formType === "newsletter" && !hasError) {
-    window.location.href = "/html/newsletter-thank-you.html";
   }
-}});
+});
 
-// Function to post contact form data to WordPress
+//-----Submitting the contact form data to wordpress backend. 
+//Sends contact form data that is gathered from the form to a WordPress endpoint via a POST request(backend), if successful submission, redirects the user to a thank-you page.
 function postContactFormDataToWordPress(name, email, subject, message) {
+  const errorSubmit = document.getElementById('errorMessageSubmit');
+
+  // Object with the form data
   const formData = {
     contactname: name,
     contactemail: email,
     contactsubject: subject,
-    contactmessage: message
+    contactmessage: message,
   };
   const proxyUrl = "https://noroffcors.onrender.com/";
-  const targetUrl = "https://james-smith.cmsbackendsolutions.com/wp-json/james-smith/v1/contact/";
+  const targetUrl =
+    "https://james-smith.cmsbackendsolutions.com/wp-json/james-smith/v1/contact/";
 
+  // Post request to the targetUrl with the formData object
   fetch(proxyUrl + targetUrl, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(formData)
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(formData),
   })
-  .then(response => response.json())
-  .then(data => {
-    if (data.message) {
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.message) {
         window.location.href = "/html/contact-thank-you.html";
-    } else {
-        console.error('Submission error:', data.error);
-    }
-    
-    // After successfully posting data, it fetch and display it on WordPress page.
-    fetch('https://james-smith.cmsbackendsolutions.com/wp-json/james-smith/v1/contact-submissions/') 
-    .then(response => response.json())
-    .then(submissions => {
-      displayFormSubmissions(submissions);
-      console.log(displayFormSubmissions);
+      } else {
+        errorSubmit.innerText = "Your submission could not be processed due to a validation error. Please check your entries and try again.";
+      console.error("Server responded with a submission error:", data.error);
+      }
     })
     .catch((error) => {
-      console.error('Error:', error);
+      errorSubmit.innerText = "We encountered a problem connecting to the server. Please check your internet connection and try again.";
+    console.error("Network or fetch error:", error);
     });
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-  });
 }
-
-
-// Function to display form submissions on your WordPress page
-function displayFormSubmissions(submissions) {
-  submissions.forEach(submission => {
-  });
-}
-
-
- 
